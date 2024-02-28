b7388189be6c1b8ebc4ebb30bacaaa0b766de1ab
tyee <2528447065@qq.com>
2024-02-28 15:50:19 +0800

    [优化]: 优化audioStore代码逻辑

--- packages/client/src/components/Music-Player.vue
+++ packages/client/src/components/Music-Player.vue
@@ -1,10 +1,18 @@
 <template>
     <div v-show="cardStore.currentCard.music" id="music-player-container">
-        <var-avatar
-            :class="animClass"
-            :src="cardStore.currentCard.imgs[0]"
-            @click="() => toggle()"
-        />
+        <var-space>
+            <var-avatar
+                v-show="lastMusicIcon && lastMusicIcon !== cardStore.currentCard.imgs[0]"
+                :class="lastAnimClass"
+                :src="lastMusicIcon"
+                @click="() => toggle()"
+            />
+            <var-avatar
+                :class="currentAnimClass"
+                :src="cardStore.currentCard.imgs[0]"
+                @click="() => toggle()"
+            />
+        </var-space>
     </div>
 </template>
 
@@ -15,10 +23,16 @@ import { getMusicUrl } from '@hello-world/api'
 import { computed } from 'vue'
 import { watchEffect } from 'vue'
 import { useLock } from '@hello-world/hooks'
+import { ref } from 'vue'
 
+//Store
 const audioStore = useAudioStore()
 const cardStore = useCardStore()
 
+//ICONS
+const lastMusicIcon = ref('')
+
+//触发音频
 watchEffect(async () => {
     if (cardStore.currentCard.music) {
         const url = await getMusicUrl(
@@ -27,27 +41,32 @@ watchEffect(async () => {
         )
         //说明是不一样的两首bgm
         locked()
-        if (url !== audioStore.audio.src) {
+        if (url !== audioStore.currentAudio.source.src) {
             audioStore.load(url)
         }
     }
 })
-
 const { locked, unlock } = useLock(
     () => (audioStore.loadTarget = audioStore.loadTarget === 'pre' ? 'next' : 'pre'),
     true
 )
 function toggle() {
     if (audioStore.loadTarget === 'pre') {
-        audioStore.stop(audioStore.nextAudio)
+        audioStore.stop('next')
     } else {
-        audioStore.stop(audioStore.preAudio)
+        audioStore.stop('pre')
     }
+    lastMusicIcon.value = cardStore.currentCard.imgs[0]
     audioStore.toggle()
     unlock()
 }
-const animClass = computed(() => {
-    return audioStore.isPaused ? '' : 'music-player-anim'
+
+//动画class
+const lastAnimClass = computed(() => {
+    return audioStore.currentAudio.paused ? '' : 'rotate-anim'
+})
+const currentAnimClass = computed(() => {
+    return audioStore.currentAudio.paused ? '' : 'rotate-anim'
 })
 </script>
 
@@ -56,10 +75,13 @@ const animClass = computed(() => {
     padding: 10px;
     display: flex;
 }
-.music-player-anim {
-    animation: scroll 5s linear infinite;
+.rotate-anim {
+    animation: rotate 5s linear infinite;
 }
-@keyframes scroll {
+.slide-anim {
+    animation: slide 5s linear;
+}
+@keyframes rotate {
     /*以百分比来规定改变发生的时间 也可以通过"from"和"to",等价于0% 和 100%*/
     0% {
         /*rotate(2D旋转) scale(放大或者缩小) translate(移动) skew(翻转)*/
@@ -67,6 +89,17 @@ const animClass = computed(() => {
     }
     100% {
         transform: rotate(360deg);
+    }
+}
+
+@keyframes slide {
+    /*以百分比来规定改变发生的时间 也可以通过"from"和"to",等价于0% 和 100%*/
+    0% {
+        /*rotate(2D旋转) scale(放大或者缩小) translate(移动) skew(翻转)*/
+        transform: translateX(0);
+    }
+    100% {
+        transform: translateX(200);
     }
 }
 </style>
--- packages/client/src/stores/audio.ts
+++ packages/client/src/stores/audio.ts
@@ -1,58 +1,70 @@
 import { defineStore } from 'pinia'
+import { reactive } from 'vue'
 import { ref } from 'vue'
 
 export const useAudioStore = defineStore('audio-store', () => {
     const preAudio = new Audio('')
-    const nextAudio = new Audio('') //作为缓存
-    const audio = ref(preAudio)
+    const nextAudio = new Audio('')
     preAudio.loop = true
     nextAudio.loop = true
-    const isInit = ref(false)
-    const isPaused = ref(true)
-    const loadTarget = ref<'pre' | 'next'>('pre') //一开始指向pre
+    type AudioTarget = 'pre' | 'next'
+    const currentAudio = reactive({
+        target: 'pre' as AudioTarget | '',
+        source: preAudio,
+        paused: true
+    })
+    //即将装载的目标
+    const loadTarget = ref<AudioTarget>('pre') //一开始指向pre
 
     // 要确保链接是https的,不然无法加载
-    function load(src: string) {
-        isInit.value = true
-        if (loadTarget.value === 'pre') {
-            audio.value = preAudio
+    function load(src: string, target: AudioTarget = loadTarget.value) {
+        currentAudio.source = target === 'pre' ? preAudio : nextAudio
+        currentAudio.source.src = src
+        currentAudio.target = target
+    }
+
+    async function play(target?: AudioTarget) {
+        if (target === 'pre') {
+            await preAudio.play()
+        } else if (target === 'next') {
+            await nextAudio.play()
         } else {
-            audio.value = nextAudio
+            await currentAudio.source.play()
+            currentAudio.paused = false
         }
-        audio.value.src = src
     }
 
-    async function swap(nextSrc: string) {
-        if (!isInit.value) return
-        stop()
-        load(nextSrc)
-        await play()
-    }
-
-    async function play(target: HTMLAudioElement = audio.value) {
-        if (!isInit.value) return
-        await target.play()
-        isPaused.value = false
-    }
-
-    function pause(target: HTMLAudioElement = audio.value) {
-        if (!isInit.value) return
-        target.pause()
-        isPaused.value = true
-    }
-
-    function stop(target: HTMLAudioElement = audio.value) {
-        if (!isInit.value) return
-        target.pause()
-        target.currentTime = 0
-        isPaused.value = true
-    }
-
-    async function toggle(target: HTMLAudioElement = audio.value) {
-        if (target.paused) {
-            await play(target)
+    function pause(target?: AudioTarget) {
+        if (target === 'pre') {
+            preAudio.pause()
+        } else if (target === 'next') {
+            nextAudio.pause()
         } else {
-            pause(target)
+            currentAudio.source.pause()
+            currentAudio.paused = true
+        }
+    }
+
+    function stop(target?: AudioTarget) {
+        if (target === 'pre') {
+            preAudio.pause()
+            preAudio.currentTime = 0
+        } else if (target === 'next') {
+            nextAudio.pause()
+            nextAudio.currentTime = 0
+        } else {
+            currentAudio.source.pause()
+            currentAudio.source.currentTime = 0
+        }
+    }
+
+    async function toggle(target?: AudioTarget) {
+        if (target === 'pre') {
+            preAudio.paused ? play(target) : pause(target)
+        } else if (target === 'next') {
+            nextAudio.paused ? play(target) : pause(target)
+        } else {
+            currentAudio.paused ? play() : pause()
         }
     }
 
@@ -62,11 +74,8 @@ export const useAudioStore = defineStore('audio-store', () => {
         pause,
         stop,
         toggle,
-        swap,
-        isPaused,
-        isInit,
         loadTarget,
-        audio,
+        currentAudio,
         nextAudio,
         preAudio
     }
